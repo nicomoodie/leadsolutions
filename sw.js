df02256f8d4d6848236bddc05fa0e1a4b374a69b
nicomoodie <nicomoodie1@gmail.com>
2026-04-26 02:21:01 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,31 +1,18 @@
-const CACHE = 'ls-crm-v1';
-const ASSETS = [
-  '/leadsolutions/crm.html',
-  '/leadsolutions/manifest.json'
-];
+const CACHE = 'ls-crm-v2';
 
 self.addEventListener('install', function(e) {
-  e.waitUntil(
-    caches.open(CACHE).then(function(cache) {
-      return cache.addAll(ASSETS);
-    })
-  );
   self.skipWaiting();
 });
 
 self.addEventListener('activate', function(e) {
   e.waitUntil(
     caches.keys().then(function(keys) {
-      return Promise.all(keys.filter(function(k){ return k !== CACHE; }).map(function(k){ return caches.delete(k); }));
+      return Promise.all(keys.map(function(k){ return caches.delete(k); }));
     })
   );
   self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-  e.respondWith(
-    fetch(e.request).catch(function() {
-      return caches.match(e.request);
-    })
-  );
+  e.respondWith(fetch(e.request));
 });
